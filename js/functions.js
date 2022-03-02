4459ac3baea30b23ce64213d12829d1262bc52f3
Marcin-Matczak <marcorn.88@gmail.com>
2022-03-02 09:34:17 +0100

    Added checking function for player input and player move

--- js/functions.js
+++ js/functions.js
@@ -10,3 +10,12 @@ function clearMessages() {
 
 const randomNumber = Math.floor(Math.random() * 3 + 1);
 
+let playerInput = Number(prompt('Choose your move: 1- rock, 2- paper, 3-scissors'));
+
+const unknownMoveAlert = playerInput => {
+    if ( playerInput = !NaN || playerInput < 1 || playerInput > 3 ) {
+        alert('You entered an invalid value. Please, try again.');
+    }
+}
+
+console.log(`ruch playera: ${playerInput}`);
--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
+/* Computer move */
 
 let computerMove = 'Unknown movement';
 
-if ( randomNumber == 1) {
+if (randomNumber == 1) {
     computerMove = 'rock';
-} else if ( randomNumber == 2 ) {
+} else if (randomNumber == 2) {
     computerMove = 'paper';
-} else if ( randomNumber == 3 ) {
+} else if (randomNumber == 3) {
     computerMove = 'scissors';
 } else {
     computerMove = 'Unknow movement';
 }
 
 printMessage(`Your opponent's move is: ${computerMove}`);
+
+/* Player move */
+
+if (playerInput == 1) {
+    playerInput = 'rock';
+    printMessage(`Your move is: ${playerInput}`);
+} else if (playerInput == 2) {
+    playerInput = 'paper';
+    printMessage(`Your move is: ${playerInput}`);
+} else if (playerInput == 3) {
+    playerInput = 'scissors';
+    printMessage(`Your move is: ${playerInput}`);
+} else {
+    unknownMoveAlert();
+}
+
